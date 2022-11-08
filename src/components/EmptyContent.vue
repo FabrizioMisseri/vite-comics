3004496d7b937c98d5512ec758ea34ff87ce908e
FabrizioMisseri <misserifabrizio@gmail.com>
2022-11-08 18:31:32 +0100

    add empty content component

--- src/components/EmptyContent.vue
+++ src/components/EmptyContent.vue
@@ -4,8 +4,24 @@
 </script>
 
 <template>
+    <section>
+        <p>
+            ADRIANO HA DETTO DI NON COPIARE NESSUNO
+        </p>
+    </section>
 </template>
 
 <style lang="scss" scoped>
+section {
+    width: 100%;
+    padding: 2rem;
+    background-color: black;
 
+    // debug paragraph
+    P {
+        text-align: center;
+        font-size: 2rem;
+        color: white;
+    }
+}
 </style>
